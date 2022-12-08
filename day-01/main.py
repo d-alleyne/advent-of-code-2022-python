6b6c9d9af63fce86046e862d0eccdf99ebbd7419
Damien Alleyne <31164683+d-alleyne@users.noreply.github.com>
2022-12-08 13:35:42 -0400

    Use Github Co-Pilot to find the top 3 values, and their total

--- day-01/main.py
+++ day-01/main.py
@@ -15,3 +15,9 @@ if __name__ == "__main__":
         elf_number = elf.index(elf_with_most)
         print("Elf number", elf_number + 1, "has the most calories with", elf_with_most, "calories.")
 
+        # find the top 3 values of elf array
+        top_3 = sorted(elf, reverse=True)[:3]
+        # Display the top 3 values and their sum
+        print("The top 3 elves have", top_3[0], "calories,", top_3[1], "calories, and", top_3[2], "calories, for a "
+                                                                                                  "total of",
+              sum(top_3), "calories.")
